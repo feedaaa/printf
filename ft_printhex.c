c213c4afdd5be1d5ef455c1ac8ddc5e9fc1ec74d
tyyggrr05 <109667869+feedaaa@users.noreply.github.com>
2023-12-11 23:01:35 +0400

    Update ft_printhex.c

--- ft_printhex.c
+++ ft_printhex.c
@@ -14,17 +14,17 @@
 
 int	ft_printhex(unsigned long long n, char format)
 {
-	int	len;
+	int	len; // Variable to store the total length of printed characters
 
-	len = 0;
-	if (n >= 16)
+	len = 0; // initializing the variable to 0
+	if (n >= 16) // If the number is greater than or equal to 16
 	{
-		len += ft_printhex((n / 16), format);
-		len += ft_printhex((n % 16), format);
+		len += ft_printhex((n / 16), format); // Recursively print the quotient
+		len += ft_printhex((n % 16), format); // Recursively print the reminder
 	}
-	if (n >= 0 && n <= 9)
-		len += ft_printchar(n + '0');
-	else if (n >= 10 && n <= 15)
+	if (n >= 0 && n <= 9)  // Print the hexadecimal digit corresponding to the current remainder
+		len += ft_printchar(n + '0'); // Print digit 0-9
+	else if (n >= 10 && n <= 15)  // Print the hexadecimal letter A-F or a-f based on the format specifier
 	{
 		if (format == 'x')
 			len += ft_printchar(n - 10 + 'a');
@@ -32,6 +32,7 @@ int	ft_printhex(unsigned long long n, char format)
 			len += ft_printchar(n - 10 + 'A');
 	}
 	return (len);
+	// Return the total length of printed characters
 }
 
 // int main()
